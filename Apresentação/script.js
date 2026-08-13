e5a2a05c07307a8dba01ebd2a060ddfc0ed0a8f2
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-08-13 13:36:50 -0300

    Price e tags ok

---
+++ Apresentação/script.js
@@ -0,0 +1,17 @@
+//Buttons
+const TForEach = document.getElementById("ForEach");
+const Map = document.getElementById("Map");
+const Reduce = document.getElementById("Reduce");
+const Filter = document.getElementById("Filter");
+const list = document.getElementById("lista");
+
+TForEach.addEventListener("click", Produtos)
+
+function Produtos() {
+  
+}
+
+
+
+
+
